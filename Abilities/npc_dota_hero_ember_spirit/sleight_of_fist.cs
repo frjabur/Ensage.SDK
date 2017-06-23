c6b3b7a65eab4c03c08a229c991402cc63824523
frjabur <frjabur@gmail.com>
2017-06-23 15:16:59 -0300

    Update sleight_of_fist.cs

--- Abilities/npc_dota_hero_ember_spirit/sleight_of_fist.cs
+++ Abilities/npc_dota_hero_ember_spirit/sleight_of_fist.cs
@@ -13,16 +13,6 @@ namespace Ensage.SDK.Abilities.npc_dota_hero_ember_spirit
         
         public string ModifierName { get; } = "modifier_ember_spirit_sleight_of_fist_caster";
     
-            protected override float RawDamage
-        {
-            get
-            {
-                var damage = this.Ability.GetAbilitySpecialData("base_damage");
-
-                var bonus = this.Ability.GetAbilitySpecialData("attack_factor_tooltip") / 100.0f; // 70
-                damage += bonus * (this.Owner.MinimumDamage + this.Owner.BonusDamage);
-
-                return damage;
             }
     }
 }
